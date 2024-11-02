d02222d4028c418a0e6607da164fbdfdf1fec4c1
MarcinTomkiewicz <marc.tomk@gmail.com>
2024-11-02 18:53:18 +0100

    Base structure, colors and other settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './components/sections/header/header.component';
+import { SidebarComponent } from './components/sections/sidebar/sidebar.component';
+import { FooterComponent } from './components/sections/footer/footer.component';
+import { RightPanelComponent } from './components/sections/right-panel/right-panel.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, HeaderComponent, SidebarComponent, RightPanelComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
